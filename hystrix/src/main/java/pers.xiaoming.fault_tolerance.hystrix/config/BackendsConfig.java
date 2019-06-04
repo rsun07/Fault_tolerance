25abe02b32ba26353c0188fdbe9d447bf82cd717
Xiaoming Sun <rsun2014@gmail.com>
2019-06-04 10:42:41 -0700

    backend config

---
+++ hystrix/src/main/java/pers.xiaoming.fault_tolerance.hystrix/config/BackendsConfig.java
@@ -0,0 +1,30 @@
+package pers.xiaoming.fault_tolerance.hystrix.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pers.xiaoming.fault_tolerance.hystrix.backends.HttpClient;
+import pers.xiaoming.fault_tolerance.hystrix.backends.HttpClientImpl;
+
+@Configuration
+public class BackendsConfig {
+
+    @Value("${hotel-endpoint}")
+    private String hotelEndpoint;
+
+    @Value("${airline-endpoint}")
+    private String airlineEndpoint;
+
+    @Bean
+    @Qualifier("hotelClient")
+    public HttpClient getHotelClient() {
+        return new HttpClientImpl(hotelEndpoint, "/hotel");
+    }
+
+    @Bean
+    @Qualifier("airlineClient")
+    public HttpClient getAirlineClient() {
+        return new HttpClientImpl(hotelEndpoint, "/airline");
+    }
+}
