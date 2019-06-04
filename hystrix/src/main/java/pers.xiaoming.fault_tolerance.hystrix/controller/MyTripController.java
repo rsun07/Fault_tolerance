3ad32eff4d235b3a624ff632abfc9e583c76b220
Xiaoming Sun <rsun2014@gmail.com>
2019-06-04 12:02:40 -0700

    Finish api layer

--- hystrix/src/main/java/pers.xiaoming.fault_tolerance.hystrix/controller/MyTripController.java
+++ hystrix/src/main/java/pers.xiaoming.fault_tolerance.hystrix/controller/MyTripController.java
@@ -1,13 +1,29 @@
 package pers.xiaoming.fault_tolerance.hystrix.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pers.xiaoming.fault_tolerance.hystrix.service.AirlineService;
+import pers.xiaoming.fault_tolerance.hystrix.service.HotelService;
 
 @RestController
 @RequestMapping("/web_client")
 public class MyTripController {
 
-    public String get() {
-        return null;
+    private AirlineService airlineService;
+    private HotelService hotelService;
+
+    @Autowired
+    public MyTripController(AirlineService airlineService, HotelService hotelService) {
+        this.airlineService = airlineService;
+        this.hotelService = hotelService;
+    }
+
+    public String get(@RequestParam("trip_id") long id) {
+        String hotelInfo = hotelService.getInfo(id);
+        String airlineInfo = airlineService.getInfo(id);
+
+        return hotelInfo + " " + airlineInfo;
     }
 }
