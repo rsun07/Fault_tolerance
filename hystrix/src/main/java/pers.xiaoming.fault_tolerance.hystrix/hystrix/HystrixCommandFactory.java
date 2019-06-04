b1dc76cef73676cd40a7268c527b056b0d7eddd0
Xiaoming Sun <rsun2014@gmail.com>
2019-06-04 11:49:53 -0700

    hystrix command factory and hystrix configs

---
+++ hystrix/src/main/java/pers.xiaoming.fault_tolerance.hystrix/hystrix/HystrixCommandFactory.java
@@ -0,0 +1,42 @@
+package pers.xiaoming.fault_tolerance.hystrix.hystrix;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
+
+public class HystrixCommandFactory {
+    private final HystrixCommand.Setter hystrixCommandSetter;
+
+    public HystrixCommandFactory(String groupKey, String commandKey, HystrixOptionalConfigs configs) {
+
+        configs = configs.fillWithDefaults();
+        this.hystrixCommandSetter = HystrixCommand.Setter
+                .withGroupKey(HystrixCommandGroupKey.Factory.asKey(groupKey))
+                .andCommandKey(HystrixCommandKey.Factory.asKey(commandKey))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        .withCircuitBreakerErrorThresholdPercentage(
+                                configs.getCircuitBreakerErrorThresholdPercentage())
+                        .withCircuitBreakerRequestVolumeThreshold(
+                                configs.getCircuitBreakerRequestVolumeThreshold())
+                        .withCircuitBreakerSleepWindowInMilliseconds(
+                                configs.getCircuitBreakerSleepWindowInMillis())
+                        .withExecutionTimeoutInMilliseconds(configs.getTimeoutInMillis())
+                        .withMetricsHealthSnapshotIntervalInMilliseconds(
+                                configs.getCircuitBreakerSleepWindowInMillis())
+                        .withExecutionIsolationSemaphoreMaxConcurrentRequests(
+                                configs.getSemaphoreMaxConcurrentRequests()
+                        )
+                );
+
+        if (configs.getIsolationStrategy().equals(HystrixCommandProperties.ExecutionIsolationStrategy.THREAD)) {
+            hystrixCommandSetter.andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter()
+                    .withCoreSize(configs.getThreadPoolCoreSize())
+                    .withKeepAliveTimeMinutes(configs.getThreadKeepAliveTimeInMinutes())
+                    .withMaxQueueSize(configs.getThreadPoolQueueSize())
+                    .withQueueSizeRejectionThreshold(configs.getThreadPoolQueueSize())
+            );
+        }
+    }
+}
