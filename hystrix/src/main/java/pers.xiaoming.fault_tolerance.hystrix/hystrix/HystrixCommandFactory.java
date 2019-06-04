e6f4eaf17d15212d70e0a7063fd3e78b9a2c0801
Xiaoming Sun <rsun2014@gmail.com>
2019-06-04 11:51:42 -0700

    complete hystrix command creation method

--- hystrix/src/main/java/pers.xiaoming.fault_tolerance.hystrix/hystrix/HystrixCommandFactory.java
+++ hystrix/src/main/java/pers.xiaoming.fault_tolerance.hystrix/hystrix/HystrixCommandFactory.java
@@ -5,6 +5,9 @@ import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.netflix.hystrix.HystrixCommandKey;
 import com.netflix.hystrix.HystrixCommandProperties;
 import com.netflix.hystrix.HystrixThreadPoolProperties;
+import pers.xiaoming.fault_tolerance.hystrix.backends.HttpClient;
+
+import java.io.IOException;
 
 public class HystrixCommandFactory {
     private final HystrixCommand.Setter hystrixCommandSetter;
@@ -38,5 +41,14 @@ public class HystrixCommandFactory {
                     .withQueueSizeRejectionThreshold(configs.getThreadPoolQueueSize())
             );
         }
+    }
+
+    public HystrixCommand<String> createCommand(HttpClient client, long id) {
+        return new HystrixCommand<String>(hystrixCommandSetter) {
+            @Override
+            protected String run() throws IOException {
+                return client.get(id);
+            }
+        };
     }
 }
