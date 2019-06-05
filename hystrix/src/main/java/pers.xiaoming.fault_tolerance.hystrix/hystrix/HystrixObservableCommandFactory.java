fca4d10288d67ed086f53cf16eadb2622027b87c
Xiaoming Sun <rsun2014@gmail.com>
2019-06-05 16:32:18 -0700

    Implement Hystrix Observable command factory

---
+++ hystrix/src/main/java/pers.xiaoming.fault_tolerance.hystrix/hystrix/HystrixObservableCommandFactory.java
@@ -0,0 +1,51 @@
+package pers.xiaoming.fault_tolerance.hystrix.hystrix;
+
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.HystrixObservableCommand;
+import pers.xiaoming.fault_tolerance.common.backends.HttpClient;
+import rx.Observable;
+
+public class HystrixObservableCommandFactory {
+    private final HystrixObservableCommand.Setter hystrixCommandSetter;
+
+    private final String fallback;
+
+    public HystrixObservableCommandFactory(String groupKey, String commandKey, HystrixOptionalConfigs configs) {
+        if (configs.isEnableFallback()) {
+            this.fallback = configs.getFallback();
+        } else {
+            this.fallback = null;
+        }
+
+        configs = configs.fillWithDefaults();
+        this.hystrixCommandSetter = HystrixObservableCommand.Setter
+                .withGroupKey(HystrixCommandGroupKey.Factory.asKey(groupKey))
+                .andCommandKey(HystrixCommandKey.Factory.asKey(commandKey))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        .withCircuitBreakerErrorThresholdPercentage(
+                                configs.getCircuitBreakerErrorThresholdPercentage())
+                        .withCircuitBreakerRequestVolumeThreshold(
+                                configs.getCircuitBreakerRequestVolumeThreshold())
+                        .withCircuitBreakerSleepWindowInMilliseconds(
+                                configs.getCircuitBreakerSleepWindowInMillis())
+                        .withExecutionTimeoutInMilliseconds(configs.getTimeoutInMillis())
+                        .withMetricsHealthSnapshotIntervalInMilliseconds(
+                                configs.getCircuitBreakerSleepWindowInMillis())
+                        .withExecutionIsolationSemaphoreMaxConcurrentRequests(
+                                configs.getSemaphoreMaxConcurrentRequests()
+                        )
+                        .withFallbackEnabled(configs.isEnableFallback())
+                );
+    }
+
+    public HystrixObservableCommand<String> createCommand(HttpClient client, long id) {
+        return new HystrixObservableCommand<String>(hystrixCommandSetter) {
+            @Override
+            protected Observable<String> construct() {
+                return Observable.fromCallable(() -> client.get(id));
+            }
+        };
+    }
+}
