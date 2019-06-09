a663d6c45b6ab279844fbe2cce018f1597d862c6
Xiaoming Sun <rsun2014@gmail.com>
2019-06-09 10:22:58 -0700

    Refactor rs4j circuit breaker test and fix configs

--- resilience4j_springboot2/src/test/java/pers/xiaoming/fault_tolerance/rs4j_annotation/circuit_breaker_test/Rs4jCircuitBreakerTestConfig.java
+++ resilience4j_springboot2/src/test/java/pers/xiaoming/fault_tolerance/rs4j_annotation/circuit_breaker_test/Rs4jCircuitBreakerTestConfig.java
@@ -8,19 +8,18 @@ import org.springframework.context.annotation.Profile;
 import pers.xiaoming.fault_tolerance.common.backends.HttpClient;
 import pers.xiaoming.fault_tolerance.common.test.client.CircuitBreakerTestHotelHttpClient;
 import pers.xiaoming.fault_tolerance.common.test.client.TestAirlineDefaultValueHttpClient;
+import pers.xiaoming.fault_tolerance.rs4j_annotation.fallback_test.Rs4jTestHotelService;
 
 @Profile("rs4j-annotation-circuit-breaker-test")
 @Configuration
 public class Rs4jCircuitBreakerTestConfig {
-    static final int LOWER_ERROR_THRESHOLD_FOR_TEST = 5;
-
     // Indicates that a bean should be given preference when multiple candidates
     // are qualified to autowire a single-valued dependency.
     @Primary
     @Bean
     @Qualifier("hotelClient")
     public HttpClient getMockHotelClient() {
-        return new CircuitBreakerTestHotelHttpClient(LOWER_ERROR_THRESHOLD_FOR_TEST);
+        return new CircuitBreakerTestHotelHttpClient(Rs4jCrcuitBreakerTest.TOTAL_ROUNDS);
     }
 
     @Primary
--- resilience4j_springboot2/src/test/java/pers/xiaoming/fault_tolerance/rs4j_annotation/circuit_breaker_test/Rs4jCrcuitBreakerTest.java
+++ resilience4j_springboot2/src/test/java/pers/xiaoming/fault_tolerance/rs4j_annotation/circuit_breaker_test/Rs4jCrcuitBreakerTest.java
@@ -1,49 +1,36 @@
 package pers.xiaoming.fault_tolerance.rs4j_annotation.circuit_breaker_test;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pers.xiaoming.fault_tolerance.common.test.client.CircuitBreakerTestHotelHttpClient;
+import pers.xiaoming.fault_tolerance.common.test.executor.CircuitBreakerExecutor;
 import pers.xiaoming.fault_tolerance.rs4j_annotation.Application;
 import pers.xiaoming.fault_tolerance.rs4j_annotation.controller.MyTripController;
+
+import java.io.IOException;
 
 @ActiveProfiles("rs4j-annotation-circuit-breaker-test")
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
 @Slf4j
-@Ignore("Test takes too long")
 public class Rs4jCrcuitBreakerTest {
-    private static final int TOTAL_ROUNDS = 100;
+    static final int TOTAL_ROUNDS = 100;
+
     private static final String SHORT_CICUIT_MSG = "CircuitBreaker 'hotel' is OPEN and does not permit further calls";
 
     @Autowired
     private MyTripController myTripController;
 
+    @Autowired
+    private CircuitBreakerTestHotelHttpClient httpClient;
+
     @Test
-    public void testCircuitBreaker() {
-        int count = 0;
-        String lastMessage = null;
-
-        for (int i = 0; i < TOTAL_ROUNDS; i++) {
-            try {
-                myTripController.get(111);
-            } catch (Exception e) {
-                log.info(e.getMessage());
-                count++;
-
-                if (i == TOTAL_ROUNDS - 1) {
-                    lastMessage = e.getMessage();
-                }
-            }
-        }
-
-        Assert.assertEquals(SHORT_CICUIT_MSG, lastMessage);
-        Assert.assertTrue(count >=
-                TOTAL_ROUNDS * (1 - Rs4jCircuitBreakerTestConfig.LOWER_ERROR_THRESHOLD_FOR_TEST));
+    public void testCircuitBreaker() throws InterruptedException {
+        CircuitBreakerExecutor.execute(TOTAL_ROUNDS, SHORT_CICUIT_MSG, httpClient, myTripController::get);
     }
 }
